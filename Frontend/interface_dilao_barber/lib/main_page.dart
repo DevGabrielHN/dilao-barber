ae3cd37a9ebef142b70d00fedbbfa77fd8b359e1
Gabriel Nascimento <gabrielhnasc@outlook.com>
2022-09-18 15:49:06 -0300

    criando pastas

---
+++ Frontend/interface_dilao_barber/lib/main_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:interface_dilao_barber/view/widgets/navbar.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavBar(),
+      appBar: AppBar(
+        title: Text("Dilão Barber"),
+      ),
+    );
+  }
+}
